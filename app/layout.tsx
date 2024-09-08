74862ae48d072b190b0ee975f97143fea698f81f
Jason Geiger <jason.phil.geiger@gmail.com>
2024-09-08 13:49:25 -0600

    Removed unnecessary import

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,4 @@
 import type { Metadata } from "next";
-import localFont from "next/font/local";
 import "./globals.css";
 
 export const metadata: Metadata = {
@@ -16,7 +15,7 @@ export default function RootLayout({
     <html lang="en">
       <body>
         <header>
-          <h1 style={{textAlign:"center"}}>Phillip Jason Geiger</h1>
+          <h1 style={{ textAlign: "center" }}>Phillip Jason Geiger</h1>
         </header>
         {children}
       </body>
