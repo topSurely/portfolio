db6c319f0f9b0e81304a81a813e65e9386323156
Jason Geiger <jason.phil.geiger@gmail.com>
2024-09-26 19:27:56 -0600

    Some extra content

--- app/page.tsx
+++ app/page.tsx
@@ -22,8 +22,10 @@ enum ProjectSelection {
 export default function Home() {
   const [jsEnabled, setJSEnabled] = useState<boolean>(false);
   const [modalOpen, setModalOpen] = useState<boolean>(false);
-  const [modalVideoID, setVideoID] = useState<string>("")
+  const [modalVideoID, setVideoID] = useState<string>("");
+  const [hoveringMeLink, setHoveringMeLink] = useState<boolean>(false);
   const [selectedProjects, setSelectedProjects] = useState<ProjectSelection | undefined>();
+  const [showFunny, setShowFunny] = useState<boolean>(false);
   const OpenModal = (videoID: string) => {
     setModalOpen(true);
     setVideoID(videoID);
@@ -63,8 +65,9 @@ export default function Home() {
           <h1>Okay, here goes</h1>
           <p>{"Hello, I'm Jason. I'm a self-taught software developer with industry experience and a passion for video games. I’m skilled at troubleshooting and solving complex problems, always eager to learn and adapt to new challenges. Feel free to explore my work, and reach out at"} <Link href={"mailto:portfolio@surely.mozmail.com"}>portfolio@surely.mozmail.com</Link></p>
         </div>
-        <div className={`${styles.infobox}`}>
+        <div className={`${styles.infobox}`} style={{ position: "relative" }}>
           <h1>Skills</h1>
+
           <div className={`${styles.grid} ${styles.skillGrid}`}>
             <div>
               <h2>Languages</h2>
@@ -103,7 +106,47 @@ export default function Home() {
                 </li>
               </ul>
             </div>
+            {showFunny && <div style={{ gridColumn: "span 3", position: "relative" }}>
+
+              <div style={{ position: "absolute", border: "none", pointerEvents: "none", margin: "-1.5rem", padding: "0", width: "100%", height: "100%" }}>
+
+                <img src="./thereIam.jpg" style={{
+                  backgroundImage: "URL(./thereIam.jpg)",
+                  position: "absolute",
+                  opacity: hoveringMeLink ? "1" : "0",
+                  transition: "opacity 0.25s", backgroundSize: "contain", backgroundRepeat: "no-repeat", zIndex: "-1000",
+                  width: "100%",
+                  height: "100%"
+                }} />
+              </div>
+              <span style={{ border: "none", margin: "auto", display: "block", width: "fit-content" }}>
+                <h2 className="spinner">🤪</h2>
+              </span>
+              <ul>
+                <li>
+                  Screamed into a microphone for the <Link href="https://youtu.be/4suQ2b_q4Cc?t=16" onMouseEnter={() => {
+                    setHoveringMeLink(true)
+                  }} onMouseLeave={() => {
+                    setHoveringMeLink(false)
+                  }}>intro to Calgary Underground Film Festival 2023</Link>. It played before every film.
+                </li>
+                <li>
+                  Starred in an <Link href="https://youtu.be/Gktdvz6Qng0?t=11" onMouseEnter={() => {
+                    setHoveringMeLink(true)
+                  }} onMouseLeave={() => {
+                    setHoveringMeLink(false)
+                  }}>OPA commercial</Link> as someone who isn't happy to see someone having a better time than me.
+                  <ul>
+                    <li>I had much longer hair at that time</li>
+                  </ul>
+                </li>
+              </ul>
+
+            </div>}
           </div>
+          <p style={{ position: "absolute", bottom: "0", right: "0", fontSize: "7px" }} onClick={() => {
+            setShowFunny(true)
+          }}>???</p>
         </div>
         <div className={styles.infobox}>
           <h1>Projects</h1>
